50080459ee30b0ea6fdfc2417e09e8db35aeeb49
dwyban <David.Wyban@tektronix.com>
2019-10-02 12:24:45 -0700

    Put Tektronix Sample Source Code License to top of source files

--- Oscilloscopes/PerformanceScopes/src/CurvestreamExample/CurvestreamExample-IVI-VISA.NET/CurvestreamExample-IVI-VISA.NET/frmMain.cs
+++ Oscilloscopes/PerformanceScopes/src/CurvestreamExample/CurvestreamExample-IVI-VISA.NET/CurvestreamExample-IVI-VISA.NET/frmMain.cs
@@ -1,4 +1,24 @@
-﻿/* Title: Curvestream Usage Example - IVI VISA.NET
+﻿/* TEKTRONIX SAMPLE SOURCE CODE LICENSE AGREEMENT
+ * 
+ * Source code written by Tektronix, Inc. or its affiliates (“Tektronix”) that
+ * is designated as a “sample,” “example,” “sample code,” or any similar
+ * designation will be considered “Sample Source Code.” Tektronix grants you a
+ * license to download, reproduce, display, distribute, modify, and create
+ * derivative works of Tektronix Sample Source Code, only for use in or with
+ * Tektronix products. You may not remove or alter any copyright notices or
+ * trademarks.
+ * 
+ * SAMPLE SOURCE CODE IS PROVIDED “AS-IS,” WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES OF ANY KIND, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT
+ * OF INTELLECTUAL PROPERTY. IN NO EVENT SHALL TEKTRONIX, ITS AFFILIATES,
+ * OFFICERS, EMPLOYEES, DIRECTORS, AGENTS, SUPPLIERS, OR OTHER THIRD PARTIES BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THIS SAMPLE
+ * SOURCE CODE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* Title: Curvestream Usage Example - IVI VISA.NET
  * 
  * Date: 2019-10-02
  * 
@@ -21,8 +41,6 @@
  * MSO73304DX FW 10.9.1
  * Ethernet VXI-11 Interface
  * 
- * Tektronix provides the following example "AS IS" without any guarantees
- * or support.  This example is for instructional guidance only.
 */
 
 using System;
--- Oscilloscopes/PerformanceScopes/src/CurvestreamExample/CurvestreamExample-NI-VISA/CurvestreamExample/frmMain.cs
+++ Oscilloscopes/PerformanceScopes/src/CurvestreamExample/CurvestreamExample-NI-VISA/CurvestreamExample/frmMain.cs
@@ -1,4 +1,24 @@
-﻿/* Title: Curvestream Example
+﻿/* TEKTRONIX SAMPLE SOURCE CODE LICENSE AGREEMENT
+ * 
+ * Source code written by Tektronix, Inc. or its affiliates (“Tektronix”) that
+ * is designated as a “sample,” “example,” “sample code,” or any similar
+ * designation will be considered “Sample Source Code.” Tektronix grants you a
+ * license to download, reproduce, display, distribute, modify, and create
+ * derivative works of Tektronix Sample Source Code, only for use in or with
+ * Tektronix products. You may not remove or alter any copyright notices or
+ * trademarks.
+ * 
+ * SAMPLE SOURCE CODE IS PROVIDED “AS-IS,” WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES OF ANY KIND, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT
+ * OF INTELLECTUAL PROPERTY. IN NO EVENT SHALL TEKTRONIX, ITS AFFILIATES,
+ * OFFICERS, EMPLOYEES, DIRECTORS, AGENTS, SUPPLIERS, OR OTHER THIRD PARTIES BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THIS SAMPLE
+ * SOURCE CODE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* Title: Curvestream Example
  * 
  * Date: 2015-08-17
  * 
@@ -19,8 +39,6 @@
  * MSO5204B FW 7.5.0
  * Ethernet VXI-11 Interface
  * 
- * Tektronix provides the following example "AS IS" without any guarantees
- * or support.  This example is for instructional guidance only.
 */
 
 using System;

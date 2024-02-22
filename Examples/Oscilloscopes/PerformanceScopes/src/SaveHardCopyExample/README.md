acdf8dbbc5865eb4a9891f9c831b4e115ead3c4a
David Wyban <david.wyban@tek.com>
2024-02-22 14:29:00 -0800

    Updated Save HardCopy example for MSO/DPO 5k 7k 70k
    Updated example to change the temp file location to a location that Windows 10 won't deny permissions.  The previous location would cause a GDI+ error to show up on the scope because it could not write to the set temp location.

--- Examples/Oscilloscopes/PerformanceScopes/src/SaveHardCopyExample/README.md
+++ Examples/Oscilloscopes/PerformanceScopes/src/SaveHardCopyExample/README.md
@@ -3,6 +3,22 @@ Original Attribution: Dave W. - Tektronix Applications
 
 This example demonstrates how to use Python along with PyVISA to save hard copy screen images to the PC from MSO/DPO 5000, 7000 and 70000 Series Scopes.
 
+## Development Environment:
+Python 3.4, PyVisa 1.8, NI-VISA 2015, Windows 8.1
+
+## Compatible Instruments:
+MSO/DPO 5000(B), 7000(C) & 70000(B)(C)(D)(DX)(SX) Series Oscilliscopes
+
+## Compatible Interfaces:
+* USB
+* Ethernet
+
+## Changelog:
+### 2024-02-22:
+Updated temporary file path for screenshot to be compatible with Windows 10 security settings
+### 2016-05-23:
+Original Revision
+
 <!-- markdown-link-check-disable -->
 Resources
 ---------
--- Examples/Oscilloscopes/PerformanceScopes/src/SaveHardCopyExample/SaveHardcopyPerformance_example.py
+++ Examples/Oscilloscopes/PerformanceScopes/src/SaveHardCopyExample/SaveHardcopyPerformance_example.py
@@ -3,13 +3,18 @@
 # Purpose:  This example demonstrates how to save a hard copy screen image from
 #   the scope to the PC.
 #
-# Created:  2016-05-23
-#
 # Development Environment: Python 3.4, PyVisa 1.8, NI-VISA 2015, Windows 8.1
 #
-# Compatible Instruments: MSO/DPO 5000, 7000 & 70000 Series Oscilliscopes
+# Compatible Instruments: MSO/DPO 5000(B), 7000(C) & 70000(B)(C)(D)(DX)(SX) Series Oscilliscopes
 #
 # Compatible Interfaces:  USB, Ethernet
+#
+# Changelog:
+#   2024-02-22:
+#     Updated temporary file path for screenshot to be compatible with Windows 10 security settings
+#
+#   2016-05-23:
+#     Original Revision
 #
 # Tektronix provides the following example "AS IS" with no support or warranty.
 #
@@ -20,8 +25,8 @@ import visa # https://pyvisa.readthedocs.org/en/stable/
 
 # Modify the following lines to configure this script for your instrument
 #==============================================
-visaResourceAddr = 'MSO5204B'
-fileSaveLocation = r'C:\Temp\\' # Folder on your PC where to save image
+visaResourceAddr = 'TCPIP::192.168.1.100::inst0:INSTR'
+fileSaveLocation = 'C:\\Users\\Public\\Pictures\\' # Folder on your PC where to save image
 #==============================================
 
 rm = visa.ResourceManager()
@@ -34,12 +39,13 @@ scope.write("EXPort:FORMat PNG")
 
 # Set where the file will be saved on the scope's hard drive.  This is not
 # where it will be saved on your PC.
-scope.write("HARDCopy:FILEName \"C:\\Temp.png\"")
+tempFilePath = "\"C:\\Users\\Tek_local_admin\\Pictures\\Temp.png\""
+scope.write("HARDCopy:FILEName " + tempFilePath)
 
 scope.write("HARDCopy STARt")
 
 # Read the image file from the scope's hard drive
-scope.write("FILESystem:READFile \"C:\\Temp.png\"")
+scope.write("FILESystem:READFile " + tempFilePath)
 imgData = scope.read_raw()
 
 # Generate a filename based on the current Date & Time
@@ -52,7 +58,7 @@ imgFile.write(imgData)
 imgFile.close()
 
 # Delete the image file from the scope's hard drive.
-scope.write("FILESystem:DELEte \"C:\\Temp.png\"")
+scope.write("FILESystem:DELEte " + tempFilePath)
 
 scope.close()
 rm.close()
